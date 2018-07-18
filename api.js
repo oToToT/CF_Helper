37f317d63332dc3a69b80326aa40f8b32db4095b
Tommy <ty1208chiang@gmail.com>
2018-07-18 21:27:25 +0800

    add cache in cf api

--- api.js
+++ api.js
@@ -1,8 +1,34 @@
 const fetch = require('node-fetch');
 const queryString = require('query-string');
-const crypto = require('crypto');
 const randomstring = require('randomstring');
-const sha512 = crypto.createHash('sha512');
+const crypto = require('crypto');
+
+const CfCache = function() {
+    let dataStore = {}, CACHED_TIME = 600000;
+    return {
+        setCacheTime: function(time) {
+            CACHED_TIME = time;
+        },
+        check: function(key) {
+            if(typeof dataStore[key] === 'undefined') return false;
+            const curTime = new Date();
+            if(curTime - dataStore[key].updTime >= CACHED_TIME) {
+                delete dataStore[key];
+                return false;
+            }
+            return true;
+        },
+        get: function(key) {
+            return dataStore[key].value;
+        },
+        update: function(key, data) {
+            const curTime = new Date();
+            dataStore[key] = { 'updTime': curTime, 'value': data };
+        }
+    }
+}
+
+const cfcache = new CfCache();
 
 const Codeforces = function() {
     let keys = {
@@ -10,184 +36,170 @@ const Codeforces = function() {
         secret: ""
     };
     async function ApiCall(method, params) {
+        // check cache
+        const cache_key = `${method}?${queryString.stringify(params)}`;
+        if(cfcache.check(cache_key))
+            return cfcache.get(cache_key);
+        // authorize cf api
         const rand = randomstring.generate(6);
         params.apiKey = keys.key;
         params.time = Math.floor(Date.now() / 1000);
-        sha512.update(`${rand}/${method}?${queryString.stringify(params)}#${keys.secret}`);
-        params.apiSig = rand+sha512.digest('hex');
+        const hasher = crypto.createHash('sha512');
+        hasher.update(`${rand}/${method}?${queryString.stringify(params)}#${keys.secret}`);
+        params.apiSig = rand+hasher.digest('hex');
+        // fetch data
         const url = `https://codeforces.com/api/${method}?${queryString.stringify(params)}`;
+        console.log(`fetching ${url}`);
         const result = await fetch(url, {
             method: 'GET', 
             cache: 'reload',
             referrer: 'no-referrer'
         }).then((res)=>res.json());
+        // check result type
         if(result.status === "FAILED") {
             throw new Error(result.comment);
         } else if(result.status === "OK") {
+            cfcache.update(cache_key, result.result);
+            console.log("success");
             return result.result;
         } else {
             throw new Error("fail calling api with unknown reason");
         }
     }
     return {
-        setApiKeys: function(key, secret) {
+        setApiKeys: async function(key, secret) {
             keys.key = key;
             keys.secret = secret;
         },
         blogEntry: {
-            comments: function(blogEntryId) {
+            comments: async function(blogEntryId) {
                 if(typeof blogEntryId === 'number')
                     blogEntryId = {'blogEntryId': blogEntryId};
                 if(typeof blogEntryId !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall("blogEntry.comments", blogEntryId);
             },
-            view: function(blogEntryId) {
+            view: async function(blogEntryId) {
                 if(typeof blogEntryId === 'number')
                     blogEntryId = {'blogEntryId': blogEntryId};
                 if(typeof blogEntryId !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall("blogEntry.view", blogEntryId);
             }
         },
         contest: {
-            hacks: function(contestId) {
+            hacks: async function(contestId) {
                 if(typeof contestId === 'number')
                     contestId = {'contestId': contestId}
                 if(typeof contestId !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall("contest.hacks", contestId);
             },
-            list: function(gym) {
+            list: async function(gym) {
                 if(typeof gym === 'undefined')
                     gym = {};
                 if(typeof gym === 'boolean')
                     gym = {'gym': gym}
                 if(typeof gym !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall("contest.list", gym);
             },
-            ratingChanges: function(contestId) {
+            ratingChanges: async function(contestId) {
                 if(typeof contestId === 'number')
                     contestId = {'contestId': contestId}
                 if(typeof contestId !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall("contest.ratingChanges", contestId);
             },
-            standings: function(params) {
+            standings: async function(params) {
                 if(typeof params !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall("contest.standings", params);
             },
-            status: function(params) {
+            status: async function(params) {
                 if(typeof params !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall("contest.status", params);
             }
         }, 
         problemset: {
-            problems: function(params) {
+            problems: async function(params) {
                 if(typeof params === 'undefined')
                     params = {};
                 if(typeof params !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 if(Array.isArray(params.tags)) 
                     params.tags = params.tags.join(';');
                 return ApiCall('problemset.problems', params);
             },
-            recentStatus: function(params) {
+            recentStatus: async function(params) {
                 if(typeof params !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall('problemset.recentStatus', params);
             }
         },
-        recentAction: function(maxCount) {
+        recentAction: async function(maxCount) {
             if(typeof maxCount === 'undefined')
                 maxCount = 100;
             if(typeof maxCount === 'number')
                 maxCount = {'maxCount': maxCount};
             if(typeof maxCount !== 'object')
-                return new Promise((resolve, reject)=>
-                    reject('Invalid Parameters')
-                );
+                throw new Error('Invalid Parameters');
             return ApiCall('recentAction', maxCount);
         },
         user: {
-            blogEntries: function(handle) {
+            blogEntries: async function(handle) {
                 if(typeof handle === 'string')
                     handle = {'handle': handle};
                 if(typeof handle !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall('user.blogEntries', handle);
             },
-            friends: function(onlyOnline) {
+            friends: async function(onlyOnline) {
                 if(typeof onlyOnline === 'undefined')
                     onlyOnline = {};
                 if(typeof onlyOnline === 'boolean')
                     onlyOnline = {'onlyOnline': onlyOnline}
                 if(typeof onlyOnline !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall("user.friends", onlyOnline);
             },
-            info: function(handles) {
+            info: async function(handles) {
                 if(typeof handles === 'string') 
                     handles = {'handles': handles}
                 if(Array.isArray(handles))
                     handles = {'handles': handles.join(';')}
                 if(typeof handles !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall("user.info", handles);
             },
-            ratedList: function(activeOnly) {
+            ratedList: async function(activeOnly) {
                 if(typeof activeOnly === 'undefined')
                     activeOnly = {};
                 if(typeof activeOnly === 'boolean')
                     activeOnly = {'activeOnly': activeOnly}
                 if(typeof activeOnly !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall("user.ratedList", activeOnly);
             },
-            rating: function(handle) {
+            rating: async function(handle) {
                 if(typeof handle === 'string')
                     handle = {'handle': handle};
                 if(typeof handle !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall('user.rating', handle);
             },
-            status: function(params) {
+            status: async function(params) {
                 if(typeof params !== 'object')
-                    return new Promise((resolve, reject)=>
-                        reject('Invalid Parameters')
-                    );
+                    throw new Error('Invalid Parameters');
                 return ApiCall('user.status', params);
+            }
+        },
+        external: {
+            setCacheTime: function(time) {
+                if(typeof time !== 'number')
+                    throw new Error('cache time should be a number');
+                cfcache.setCacheTime(time);
             }
         }
     };
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,19 @@ const Codeforces = require('~/api.js');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    let val = "Express";
+    res.render('index');
+});
+router.get('/compare/:id1/:id2', async function(req, res, next) {
+    const id1 = req.params.id1, id2 = req.params.id2;
+    console.log(`> compare ${id1} and ${id2}`);
+    let result = {}
     try{
-        val = await Codeforces.user.info("oTo_ToT");
+        result = await Promise.all(Codeforces.external.user.acProblems(id1), 
+            Codeforces.external.user.acProblems(id2));
     }catch(error){
-        val = error;
+        result = error;
     }
-    res.render('index', { title: val });
+    res.render('compare', {one: result[0], two: result[1]});
 });
 
 module.exports = router;
