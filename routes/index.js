c6def892db3ed11c757c97d267592a774777410b
Tommy <ty1208chiang@gmail.com>
2018-07-19 18:17:36 +0800

    add compare page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Codeforces = require('~/api.js');
 
+// cache for 30 minute
+Codeforces.external.setCacheTime(30 * 60000);
+
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     res.render('index');
@@ -19,19 +22,26 @@ router.get('/compare/:id1/:id2', async function(req, res, next) {
     }catch(error){
         return res.render('compare', {'error': error});
     }
-    const only1 = [...s1].filter((x)=>{
+    const prob_comp = function(a, b) {
+        if(a.contestId === b.contestId){
+            if(a.index === b.index) return 0;
+            else if(a.index < b.index) return -1;
+            else return 1;
+        }else return a.contestId - b.contestId;
+    }
+    const one = [...s1].filter((x)=>{
         const x_name = `${x.contestId}${x.index}`;
         return !s2.find((y)=>x_name == `${y.contestId}${y.index}`);
-    });
-    const only2 = [...s2].filter((x)=>{
+    }).sort(prob_comp);
+    const two = [...s2].filter((x)=>{
         const x_name = `${x.contestId}${x.index}`;
         return !s1.find((y)=>x_name == `${y.contestId}${y.index}`);
-    })
+    }).sort(prob_comp);
     const both = [...s1].filter((x)=>{
         const x_name = `${x.contestId}${x.index}`;
         return s2.find((y)=>x_name == `${y.contestId}${y.index}`);
-    })
-    return res.render('compare', {only1, only2, both});
+    }).sort(prob_comp);
+    return res.render('compare', {id1, id2, one, two, both});
 });
 
 module.exports = router;
